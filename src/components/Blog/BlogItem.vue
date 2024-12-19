ee8a368e0a481e3835ff64b951328d5d2e19cfa5
Christyan Simbolon <sambungdata@gmail.com>
2024-12-19 14:14:47 +0700

    Blog item start to look neat

--- src/components/Blog/BlogItem.vue
+++ src/components/Blog/BlogItem.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="blog-item border p-4 rounded-lg shadow hover:shadow-lg">
-    <h3 class="mt-4 text-xl font-bold">{{ blog.title }}</h3>
-    <p class="mt-2 text-gray-600">{{ blog.excerpt }}</p>
-    <p class="mt-6 text-gray-600">{{ blog.content }}</p>
+  <div class="blog-item">
+    <!-- Blog Image -->
+    <img :src="blog.image" :alt="blog.title" class="h-40 w-full object-cover" loading="lazy" />
 
-    <!-- <router-link :to="blog.content" class="text-blue-600 hover:underline mt-4 inline-block">
-      Read More
-    </router-link> -->
+    <!-- Blog Content -->
+    <div class="p-4">
+      <!-- Blog Title -->
+      <h3 class="text-lg font-bold text-gray-800 mb-2 line-clamp-2">
+        {{ blog.title }}
+      </h3>
+
+      <!-- Blog Excerpt -->
+      <p class="text-gray-600 text-sm mb-4 line-clamp-3">
+        {{ blog.excerpt }}
+      </p>
+
+      <!-- Read More Link -->
+      <a href="#" class="text-blue-600 font-medium hover:underline text-sm"> Read More </a>
+    </div>
   </div>
 </template>
 
@@ -21,3 +32,25 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.blog-item {
+  @apply w-[320px] max-w-sm mx-auto border rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out bg-white;
+}
+
+.blog-item img {
+  @apply rounded-t-lg;
+}
+.line-clamp-2 {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+.line-clamp-3 {
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
