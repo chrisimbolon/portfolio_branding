b85a2887cca767f680fe87e90ed66ba2bd7b6c0a
Christyan Simbolon <sambungdata@gmail.com>
2025-04-21 03:37:28 +0700

    Styling on Built Upon Section-5th updating

--- src/components/Contact/BuiltUpon.vue
+++ src/components/Contact/BuiltUpon.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card max-w-4xl mx-auto mt-2 mb-8 px-6 py-4 rounded-xl shadow-md bg-gradient-to-br from-gray-100 to-gray-200">
-      <p class="text-brand-blue-2 text-lg leading-relaxed">
+      <p class="text-brand-blue-2 text-sm leading-relaxed">
         Thoughtfully designed from sketch to screen, developed in 
         <strong>VS Code</strong> using <strong>Vue.js</strong> and 
         <strong>Tailwind CSS</strong>. Visual assets are hosted on 
