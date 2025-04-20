5b60060d66276551920a36a07e8abfa27da10d94
Christyan Simbolon <sambungdata@gmail.com>
2025-04-21 02:05:44 +0700

    Styling on Built Upon Section-2nd updating

--- src/components/Contact/BuiltUpon.vue
+++ src/components/Contact/BuiltUpon.vue
@@ -1,8 +1,8 @@
 <template>
     <!-- <div class="skills-section px-5 pb-5 border border-white bg-gradient-to-b from-[#D9DADB] to-[#e5e4e2]"> -->
-      <div class="card flex flex-col max-w-4xl mx-auto m-6 p-6 rounded-md shadow-lg">
+      <div class="card flex flex-col max-w-4xl mx-auto m-2 p-2 rounded-md shadow-lg">
         <p class="flex-grow p-4 text-brand-blue-2 leading-relaxed">
-            designed both on paper and photoshop, coded in Visual Studio Code. Built on Vue.js and Tailwind CSS, images on Firebase Firestore. All text is set in Open Sans.
+            Thoughtfully designed from sketch to screen, developed in Visual Studio Code using Vue.js and Tailwind CSS. Visual assets are hosted on Firebase Firestore. Typography is set in Open Sans for a clean, modern feel
         </p>
       </div>
     <!-- </div> -->
